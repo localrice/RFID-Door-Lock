3cae2713b46c6ea9aea3c0054e3935721e4431b5
localrice <borokinjalkavish@gmail.com>
2025-11-05 13:08:36 +0530

    add: checkUID function

--- src/main.cpp
+++ src/main.cpp
@@ -20,3 +20,57 @@ void registerUID(String uid, String name, String role)
   Serial.printf("Added new UID: %s | Name: %s | Role: %s\n",
                 uid.c_str(), name.c_str(), role.c_str());
 }
+
+bool checkUID(String uid, String *name, String *role) 
+{
+  uid.trim();
+  uid.toUpperCase();
+
+  if (!LittleFS.exists("/uids.txt")) 
+  {
+    Serial.println("No UID file found."); 
+    return false;
+  }
+
+  File file = LittleFS.open("/uids.txt", "r");
+  if (!file) 
+  { 
+    Serial.println("Failed to open uid file for writing"); 
+    return; 
+  }
+
+  while (file.available()) 
+  {
+    String line = file.readStringUntil('\n');
+    line.trim();
+
+    if (line.length() == 0) continue;
+
+    // parse the csv line: UID,Name,Role
+    int firstComma = line.indexOf(',');
+    int secondComma = line.lastIndexOf(',');
+
+    if (firstComma == -1 || secondComma == -1 || firstComma == secondComma) continue;
+
+    String storedUID = line.substring(0,firstComma);
+    storedUID.trim();
+    storedUID.toUpperCase();
+
+    // checking if it exists
+    if (storedUID == uid) 
+    {
+      if (name != nullptr) 
+        *name = line.substring(firstComma + 1, secondComma);
+      if (role != nullptr) 
+        *role = line.substring(secondComma + 1);
+      
+      file.close();
+      Serial.println("UID found");
+      return true;
+    }
+  }
+
+  file.close();
+  Serial.println("UID not found");
+  return false;
+}
